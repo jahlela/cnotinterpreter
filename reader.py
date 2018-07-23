a8b63dc4c1b270de7d2d09e8e9bd4c85fa836c83
jahlela <jahlela5@gmail.com>
2018-07-23 23:22:54 +0200

    Add Types, File, and Interpreter classes with inits

--- reader.py
+++ reader.py
@@ -1,9 +1,53 @@
 # Read in file
 
-with open('./test.txt') as file:
-    content = file.readlines()
+class File(object):
+    def __init__(self, filename = ""):
+        self.filename = filename
+        self.get_file_data()
 
-content = [x.strip() for x in content]
-print content
+    def get_file_data(self):
+        if self.filename:
+            with open(self.filename, 'r') as readable_file:
+                self.readable_file = readable_file
+                self.content = self.readable_file.readlines()
+                self.lines = [x.strip() for x in self.content]
+            readable_file.close()
+    
 
-file.close()
+class Types(object):
+    def __init__(self):
+        self.primitives = {
+            'bool': 'BOOL',
+            'int': 'INT',
+            'str': 'STRING',
+            'float': 'FLOAT',
+            'long': 'LONG'
+        }
+        self.compounds = {}
+#        self.options = {**self.primitives, **self.compounds}
+
+    def detect_type(elem):
+        elem_type = type(elem)
+        if elem_type in self.primitives:
+            return self.options[elem_type]
+        else:
+            raise('Type {} not found'.format(elem_type))
+
+
+
+class Interpreter(object):
+    def __init__(self, filename = ""):
+        self.f = File(filename)
+        self.variables = {}
+        
+
+    def parse_variables(self):
+        t = Types()
+        for line in self.f.lines:
+            print line[0]
+            if line[0] in t.primitives:
+                print t.primitives[line[0]]
+            
+
+interp = Interpreter('test.txt')
+print interp.parse_variables()
