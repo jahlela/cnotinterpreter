d28c0605713cf7edb031e9e43562d12d4b0e79e4
jahlela <jahlela5@gmail.com>
2018-07-29 02:06:24 +0200

    Add compound types, Variable class and Interpreter logic

--- reader.py
+++ reader.py
@@ -6,6 +6,9 @@ class File(object):
         self.get_file_data()
 
     def get_file_data(self):
+        """
+        Returns an array of stripped lines in program
+        """
         if self.filename:
             with open(self.filename, 'r') as readable_file:
                 self.readable_file = readable_file
@@ -23,8 +26,13 @@ class Types(object):
             'float': 'FLOAT',
             'long': 'LONG'
         }
-        self.compounds = {}
-#        self.options = {**self.primitives, **self.compounds}
+        self.compounds = {
+            'list': 'LIST',
+            'dictionary': 'DICTIONARY',
+            'set': 'SET'
+        }
+        self.options = {**self.primitives, **self.compounds}
+        print(self.options)
 
     def detect_type(elem):
         elem_type = type(elem)
@@ -33,21 +41,30 @@ class Types(object):
         else:
             raise('Type {} not found'.format(elem_type))
 
+class Variable(object):
+    def __init__(self, varType, value = None):
+        self.varType = varType
+        self.value = value
 
+    def __repr__(self):
+        return f'{self.varType} {self.value}'
+        
 
 class Interpreter(object):
     def __init__(self, filename = ""):
-        self.f = File(filename)
+        self.text = File(filename).content
         self.variables = {}
-        
+        self.types = Types()
 
     def parse_variables(self):
-        t = Types()
-        for line in self.f.lines:
-            print line[0]
-            if line[0] in t.primitives:
-                print t.primitives[line[0]]
+        for line in self.text:
+            words = line.strip().split(" ")
+            varType, name, assignment = words[0:3]
+            value = (" ").join(words[3:])
+            newVar = Variable(self.types.options[varType], value)
+            self.variables[name] = newVar
+            print(self.variables)
             
 
 interp = Interpreter('test.txt')
-print interp.parse_variables()
+print(interp.parse_variables())
